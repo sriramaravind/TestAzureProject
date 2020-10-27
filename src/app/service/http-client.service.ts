f7f38842452aa50164c4faaf5d82f8a2e783092e
sriramaravind <65369583+sriramaravind@users.noreply.github.com>
2020-10-27 15:14:18 +0530

    inital Commit

---
+++ src/app/service/http-client.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { User } from "../model/user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpClientService {
+
+  constructor(
+    private httpClient:HttpClient
+  ) { 
+     }
+
+     getUsers()
+  {
+    return this.httpClient.get<User[]>('http://localhost:8080/users/get');
+  }
+
+  addUser(newUser: User) {
+    return this.httpClient.post<User>('http://localhost:8080/users/add', newUser);   
+  }
+
+  deleteUser(id) {
+    return this.httpClient.delete<User>('http://localhost:8080/users/' + id);
+  }  
+}
